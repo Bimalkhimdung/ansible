e5a7a02bbb66b7bef234dc3257c747c4ff9532dd
bimalkhimdung <bimalkhimdung@gmail.com>
2024-03-29 14:34:56 +0545

    first commit

---
+++ bare-repo.sh
@@ -0,0 +1,14 @@
+mkdir testcore-repo.git 
+cd testcore-repo.git
+git init --bare . 
+git --bare update-server-info 
+git config core.bare false
+git config receive.denycurrentbranch ignore  
+git config core.worktree /home/test/testcore-realhrsoft-backend/irealhrsoft-backend/
+
+cat > hooks/post-receive <<EOF 
+#!/bin/sh
+git checkout -f
+EOF
+ 
+chmod +x hooks/post-receive
