8c938152fa4ea9890dc25da781e1f38a59839404
bimalkhimdung <bimalkhimdung@gmail.com>
2024-09-04 16:28:21 +0545

    office

--- bare-repo.sh
+++ bare-repo.sh
@@ -1,10 +1,10 @@
-mkdir shikharinsurance-repo.git 
-cd shikharinsurance-repo.git
+mkdir nyayahealthnepal-repo.git 
+cd nyayahealthnepal-repo.git
 git init --bare . 
 git --bare update-server-info 
 git config core.bare false
 git config receive.denycurrentbranch ignore  
-git config core.worktree /home/realhr/shikharinsurance-realhrsoft-backend/irealhrsoft-backend
+git config core.worktree /home/ubuntu/nyayahealthnepal-realhrsoft-backend/irealhrsoft-backend
 
 cat > hooks/post-receive <<EOF 
 #!/bin/sh
